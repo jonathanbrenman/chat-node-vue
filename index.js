7c3f7050883e8fb83d6678dffa3b69551333849c
Jonathan Brenman <jonathan-work@Jony-MacBook-Pro-Work.local>
2019-01-22 14:42:31 -0300

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const dotenv  = require('dotenv').config(),
       express = require('express'),
-      port    = process.env.APPLICATION_PORT,
+      port    = process.env.APPLICATION_PORT || 3000,
       app     = express(),
       server  = require('http').createServer(app);
 
@@ -9,7 +9,7 @@ const dotenv  = require('dotenv').config(),
   app.use(express.static(__dirname + '/client/dist'));
 
   server.listen(port, () => {
-    console.log('Server started.');
+    console.log(`Server started on port ${port}`);
   });
 
 const io      = require('socket.io')(server),
